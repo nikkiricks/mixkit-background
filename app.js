d531213ed57e833c5535fedd14fb24a3c3127c34
Nikki Ricks <nikki.ricks@envato.com>
2021-08-25 20:41:09 +1000

    add index and app

---
+++ app.js
@@ -0,0 +1,12 @@
+console.log("working")
+const body = document.querySelector('body');
+
+function randomImage(){
+
+  var randomImageId = Math.floor(Math.random() * 560);
+  randomUrl = `https://mixkit.imgix.net/art/${randomImageId}/${randomImageId}-desktop-wallpaper.png?auto=format&fm=jpg&q=80&h=1500`
+  body.style["background-image"] = "url("+ randomUrl + ")";
+}
+
+document.addEventListener("DOMContentLoaded", randomImage);
+
